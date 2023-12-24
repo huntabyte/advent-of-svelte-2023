9b1dbb465a6626f0f488441cecba12997589b294
Hunter Johnston <johnstonhuntera@gmail.com>
2023-12-24 12:57:42 -0700

    day 3 styles

--- src/routes/day/3/+page.svelte
+++ src/routes/day/3/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import * as Card from "$lib/components/ui/card";
 	import type { Present } from "./+page.js";
 	import { Button } from "$lib/components/ui/button";
 	import { Progress } from "$lib/components/ui/progress";
+	import { Gift } from "phosphor-svelte";
 	let { data } = $props();
 
 	let backlogPresents = $state([...data.presents]);
@@ -15,28 +17,49 @@
 	});
 </script>
 
-<div class="pb-4">
-	<p>Current Load: {currentLoad}</p>
+<div class="mx-auto flex max-w-screen-sm flex-col gap-2 pb-4">
+	<span class="font-medium">Current Load: {currentLoad}</span>
 	<Progress max={MAX_LOAD} value={currentLoad} />
 </div>
-<div class="flex items-start w-full gap-4">
-	<div class="grid grid-cols-3 gap-2 border border-border rounded-md w-full p-4">
-		{#each backlogPresents as present, i (`${present.name}-${present.weight}`)}
-			{#if currentLoad + present.weight <= MAX_LOAD}
+<div class="flex w-full items-start gap-4">
+	<Card.Root class="min-w-1/2 w-full">
+		<Card.Header class="px-6 pb-2 pt-6">
+			<Card.Title>Backlog</Card.Title>
+		</Card.Header>
+		<div class="grid w-full grid-cols-3 gap-2 p-4">
+			{#each backlogPresents as present, i (`${present.name}-${present.weight}`)}
+				{#if currentLoad + present.weight <= MAX_LOAD}
+					<Button
+						variant="ghost"
+						onclick={() => sleighPresents.push(backlogPresents.splice(i, 1)[0]!)}
+						class="h-12 justify-start border border-green-400/40 hover:bg-green-900"
+					>
+						<Gift class="mr-2 h-4 w-4" />
+						<span class="sr-only">Gift</span>
+						<p>{present.name} - {present.weight}</p>
+					</Button>
+				{/if}
+			{/each}
+		</div>
+	</Card.Root>
+	<Card.Root class="min-w-1/2 w-full">
+		<Card.Header class="px-6 pb-2 pt-6">
+			<Card.Title>Sleigh</Card.Title>
+		</Card.Header>
+		<div class="grid w-full grid-cols-3 gap-2 p-4">
+			{#each sleighPresents as present, i (`${present.name}-${present.weight}`)}
 				<Button
 					variant="ghost"
-					onclick={() => sleighPresents.push(backlogPresents.splice(i, 1)[0]!)}
+					onclick={() => backlogPresents.push(sleighPresents.splice(i, 1)[0]!)}
+					class="h-12 justify-start border border-red-400/40 hover:bg-red-900"
 				>
-					{present.name} - {present.weight}
+					<Gift class="mr-2 h-4 w-4" />
+					<span class="sr-only">Gift</span>
+					<p>
+						{present.name} - {present.weight}
+					</p>
 				</Button>
-			{/if}
-		{/each}
-	</div>
-	<div class="grid grid-cols-3 border border-border rounded-md min-w-1/2 w-full p-4">
-		{#each sleighPresents as present, i (`${present.name}-${present.weight}`)}
-			<Button variant="ghost" onclick={() => backlogPresents.push(sleighPresents.splice(i, 1)[0]!)}>
-				{present.name} - {present.weight}
-			</Button>
-		{/each}
-	</div>
+			{/each}
+		</div>
+	</Card.Root>
 </div>
